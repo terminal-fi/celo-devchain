f973c8b1a0b4b5532d2eb23f8e12b715b5b87019
Zviad Metreveli <thezviad@gmail.com>
2020-11-09 12:24:39 -0500

    downgrade web3 version to match contractkit

--- src/utils.ts
+++ src/utils.ts
@@ -8,14 +8,14 @@ export function increaseTime(web3: Web3, secondsToAdd: number) {
 		throw new Error(`invalid currentProvider!`)
 	}
 	return new Promise((resolve, reject) => {
-		provider.send!({
+		provider.send({
 			jsonrpc: '2.0',
 			id: Date.now(),
 			method: 'evm_increaseTime',
 			params: [secondsToAdd],
 		}, (err) => {
 			if (err) return reject(err);
-			provider.send!({
+			provider.send({
 				jsonrpc: '2.0',
 				id: Date.now(),
 				method: 'evm_mine',
